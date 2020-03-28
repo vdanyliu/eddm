4115977dac1904e67c5f77e0b1605d7a1ac8f375
vdanyliu <fretalk@ukr.net>
2020-03-28 22:54:25 +0200

    Пункты меню добавляються и удаляються

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -4,11 +4,28 @@
 
 	use config\Config;
 	use core\Controller;
+	use core\MenuNode;
 
 	class MainController extends Controller
 	{
 		public function indexAction()
 		{
+			$test = new MenuNode("1st");
+			$test->addNewNode('root', 'Тест');
+			$test->addNewNode('root', 'Тест');
+			$test->addNewNode('root', 'Тест');
+			$test->addNewNode('root/0', 'Тест');
+			$test->addNewNode('root/1', 'Тест');
+			$test->addNewNode('root/2', 'Тест');
+			$test->addNewNode('root/3', 'Тест');
+			$test->addNewNode('root/1/0', 'Тест');
+			$test->addNewNode('root/1/11', 'Тест');
+			debug($test);
+			$test->deleteNode('root/0');
+			$test->deleteNode('root/2');
+			$test->deleteNode('root/3');
+			$test->deleteNode('root/1');
+			debug($test);
 			$arr = [];
 			$this->view->render("index page", $arr);
 		}
--- core/MenuNode.php
+++ core/MenuNode.php
@@ -6,9 +6,9 @@ namespace core;
 
 class MenuNode
 {
-	private $path;
-	private $title;
-	private $menuItems;
+	public $path;
+	public $title;
+	public $menuItems;
 
 	/**
 	 * MenuNode constructor.
@@ -21,30 +21,42 @@ class MenuNode
 		$this->title = $title;
 	}
 
-	function modifyPath($parentPath, $key) {
-		$this->path = $parentPath . '/' . $key;
+	function deleteNode($path) {
+		$arr = explode('/', $path);
+		$key = $arr[array_key_last($arr)];
+		unset($arr[array_key_last($arr)]);
+		$newPath = implode('/', $arr);
+		$node = $this->getNodeByPath($newPath);
+		unset($node->menuItems[$key]);
 	}
 
 	function addNewNode($path, $title)
 	{
-		$node = $this->getNodeByPath($path);
+		if (!$node = $this->getNodeByPath($path)) {
+			return;
+		}
 		$newNode = new self($title);
 		array_push($node->menuItems, $newNode);
 		$last_key = array_key_last($node->menuItems);
-		$node->menuItems->modifyPath($node->path, $last_key);
+		$newNode->modifyPath($node->path, $last_key);
 	}
 
-	function getNodeByPath($path) {
+	private function modifyPath($parentPath, $key) {
+		$this->path = $parentPath . '/' . $key;
+	}
+
+	private function getNodeByPath($path, $deepLevel = 0) {
 		if ($this->path == $path)
 			return $this;
 		else {
 			$arr = explode('/', $path);
-			if (!isset($arr[1]) || !isset($this->menuItems[$arr[1]]))
+			if (!isset($arr[1+$deepLevel]) || !isset($this->menuItems[$arr[1+$deepLevel]])) {
+				printf("Такого меню не существует '%s'\n", $path);
 				return NULL;
-			unset($arr[0]);
-			$newPath = implode($arr);
-			$nextNode = $this->menuItems[$arr[1]];
-			$node = $nextNode->getNodeByPath($newPath);
+			}
+			$nextNode = $this->menuItems[$arr[1+$deepLevel]];
+			$deepLevel++;
+			$node = $nextNode->getNodeByPath($path, $deepLevel);
 			return $node;
 		}
 	}
